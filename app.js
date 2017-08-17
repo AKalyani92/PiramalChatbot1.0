8e0ee5d958668e3370431ebe1e6cc03232e507ba
Aditi Kalyani <aditikalyani92@gmail.com>
2017-08-17 16:37:32 +0530

    Flow added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 var request = require('request');
+var o = require('odata');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -17,11 +18,74 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 
+bot.on('error', function(message) {
+    console.log('[error] called'+message);
+});
+
+bot.on('conversationUpdate', function (message) {
+    console.log("Called Conversation updated");
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        var isSelf = false;
+        var membersAdded = message.membersAdded
+            .map(function (m) {
+                isSelf = m.id === message.address.bot.id;
+                return (isSelf ? message.address.bot.name : m.name) || '' + ' (Id: ' + m.id + ')';
+            })
+            .join(', ');
+        if (!isSelf) {
+            console.log("not self");
+            bot.send(new builder.Message()
+                .address(message.address)
+                .text('Welcome ' + membersAdded + "! How can I help you?"));
+            bot.beginDialog(message.address,'/');
+        }
+    }
+});
+
+Date.prototype.addDays = function(days) {
+    this.setDate(this.getDate() + parseInt(days));
+    return this;
+};
+
+
+
 // Root dialog for entry point in application
 bot.dialog('/', [
     function (session,args, next) {
-        session.send("Hi");
-    }
+        result = args || {};
+        if (result == undefined || result.response == undefined) {
+            userAddress = session.message.address;
+            builder.Prompts.text(session, "Hi " + session.message.user.name + " How can i help you?");
+        }
+        else if (result.response == "NU") {
+            builder.Prompts.text(session, "You can say : 'Analytics'");
+        }
+    },
+    function (session, results) {
 
+        RootMenu(session,results);
+    },
+    function (session,results) {
+        console.log("root final : " + results.response);
+        RootMenu(session,results);
+    }
 ]);
+
+function RootMenu(session,results) {
+
+    if (results.response.toUpperCase().indexOf("ANALYTICS") !== -1) {
+        session.beginDialog('/Analytics');
+    }
+    else if (results.response.toUpperCase().indexOf("NO") != -1) {
+        session.send("Ok then " + session.message.user.name + ", Goodbye :)");
+        session.endDialog();
+    }
+    else if (results.response.toUpperCase().indexOf("YES") != -1) {
+        session.beginDialog('/Analytics');
+    } else {
+        session.send("Not Trained...");
+        session.beginDialog('/', {response: 'NU'});
+    }
+}
+
 
